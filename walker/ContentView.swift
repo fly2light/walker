0033ed97fd3b4d2fc40d0517794681288b05a8f9
fly2light <1187266380@qq.com>
2020-06-20 09:47:56 +0800

    Initial Commit

---
+++ walker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  walker
+//
+//  Created by 付阳 on 2020/6/20.
+//  Copyright © 2020 付阳. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
